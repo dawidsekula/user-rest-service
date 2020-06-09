972760cb6fa437c29280e73dd688fb589b08c8d2
dawidsekula <47383542+dawidsekula@users.noreply.github.com>
2020-06-09 10:19:06 +0200

    Project beginning

---
+++ src/main/java/site/transcendence/userrestservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package site.transcendence.userrestservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserRestServiceApplication.class);
+    }
+
+}
